5c43470c1f5dc4901a648b0223f14edaec038f3f
Steopa2001 <moraru495@gmail.com>
2025-07-03 15:59:03 +0200

    feat: add a teacher name to the end of array

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -32,6 +32,8 @@ console.log('First Teacher removed:', firstTeacher);
 console.log('Teachers now:', teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push(`Vanessa`);
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 

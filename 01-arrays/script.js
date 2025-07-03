d25358cefbd315b854fc6f2aee7373441e8238ba
Steopa2001 <moraru495@gmail.com>
2025-07-03 14:59:34 +0200

    feat: changed the value of the variable fourTeacher

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -7,12 +7,14 @@ const teachers = [
   'Lewis',
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
+  console.log(teachers)
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
-
+const fourthTeacher = teachers[3];
+console.log('4th Teacher:', fourthTeacher);
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
